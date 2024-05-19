c216251dd68f64cfe6c77f6fb7ee946d23aaec14
Jesus Zazueta <jjzazuet@gmail.com>
2024-05-19 15:30:24 -0400

    - Status validation fix.

--- tk-sdr/src/main/java/io/vacco/tokoeka/util/TkCommand.java
+++ tk-sdr/src/main/java/io/vacco/tokoeka/util/TkCommand.java
@@ -131,6 +131,9 @@ public class TkCommand {
   }
 
   public static boolean isKiwiOk(String key, String value) {
+    if (key == null) { // not a control message
+      return true;
+    }
     var pOk = Objects.equals(key, badp) && Objects.equals(value, "0");
     var bus = Objects.equals(key, too_busy) && Objects.equals(value, "0");
     return pOk || bus;
