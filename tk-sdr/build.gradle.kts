bad35f68cda8482c0ff3c4e9458120cfe1167f0a
Jesus Zazueta <jjzazuet@gmail.com>
2024-10-10 10:43:39 -0400

    - Dependency update.

--- tk-sdr/build.gradle.kts
+++ tk-sdr/build.gradle.kts
@@ -6,7 +6,7 @@ val api by configurations
 
 dependencies {
   api(project(":tk-schema"))
-  api("org.slf4j:slf4j-api:2.0.6")
-  testImplementation("io.vacco.shax:shax:2.0.6.0.1.0")
+  api("org.slf4j:slf4j-api:2.0.16")
+  testImplementation("io.vacco.shax:shax:2.0.16.0.1.2")
   testImplementation("com.google.code.gson:gson:2.10.1")
 }
